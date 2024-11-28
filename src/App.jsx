1ccf93d7bf2ad3d75051fb1b3e163847f66ac7fb
Facundo Vigil <facuvigil.23@gmail.com>
2024-11-28 18:53:03 -0300

    segunda vuelta

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar.jsx'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer.jsx'
-function App() {
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
+import Card from './components/Card';
+function App() {
 
   return (
     <BrowserRouter>
-    <NavBar/>
-    <Routes> 
-      <Route path='/' element={<ItemListContainer/>}/>
-      <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
-      <Route path='/detail/:productId' element={<ItemDetailContainer/>}/>
-      <Route path='*' element={<h1>404</h1>}/>
-    </Routes>
+      <NavBar title="Tienda Coder" />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/detail/:productId" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/contacto" element={<Card />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+import { getProducts, getProductsByCategory } from "../../asyncMock";
+import { useParams } from "react-router-dom";
+import ItemList from "../ItemList/ItemList";
 
 export default function ItemListContainer() {
-    return (    
-        <div>   
-            <h2>lista de productos</h2>
-            <hr />
+    const [products, setProducts] = useState([])
+    const { categoryId } = useParams()
+    useEffect(() => {
+        const asyncFunction = categoryId ? getProductsByCategory : getProducts
+        asyncFunction(categoryId)
+            .then(data => setProducts(data))
+    }, [categoryId])
+
+    return (
+        <div>
+            <h2>Bienvenidos a la Tienda</h2>
+            <ItemList products={products} />
         </div>
-    )
+    );
 }
